f098a653f0ec44d8f37c1d3a27e024cf70536748
anivegesana <anirudh.vegesana@gmail.com>
2023-03-14 20:33:59 -0700

    Download comments

--- dataset/clean_csvs.py
+++ dataset/clean_csvs.py
@@ -7,12 +7,12 @@ import os
 dataset_folder = os.path.dirname(__file__)
 os.chdir(dataset_folder)
 
-def clean_scrape(df_use):
+def clean_scrape(df_use, body='selftext'):
     # Remove duplicate entries that may have been recieved due to multiple downloads
     df_use = df_use.drop_duplicates('id')
     # Remove any deleted or removed posts
     gone_list = ["[deleted]","[removed]",""]
-    df_use = df_use[df_use['selftext'].isin(gone_list)==False]
+    df_use = df_use[df_use[body].isin(gone_list)==False]
     print("After removing deleted posts, there are " +  str(len(df_use)) + " posts left.")
     return(df_use)
 
@@ -34,5 +34,15 @@ if os.path.exists("posts_training.csv"):
     print("There are now " +  str(len(grand)) + " cleaned training posts.")
 
     grand.to_csv("posts_clean.csv",index=False)
+else:
+    print("No training posts available. Run `python download_training_posts.py` if you want them.")
+
+if os.path.exists("comments.csv"):
+    raw = pd.read_csv("comments.csv", on_bad_lines='skip')
+    grand = clean_scrape(raw, body='body')
+
+    print("There are now " +  str(len(grand)) + " cleaned comments.")
+
+    grand.to_csv("comments_clean.csv",index=False)
 else:
     print("No training posts available. Run `python download_training_posts.py` if you want them.")
--- dataset/utils.py
+++ dataset/utils.py
@@ -1,4 +1,6 @@
 import enum
+import praw
+import numpy as np
 
 class Flair(str, enum.Enum):
   YTA = "Asshole"
@@ -52,4 +54,43 @@ class PostTable:
         'num_comments':self.num_comments,
         'edited':self.edited,
         'selftext':self.self_texts
+    }
+
+class CommmentTable:
+  __slots__ = (
+    'id',
+    'link_id',
+    'post_id',
+    'score',
+    'reward',
+    'regret',
+    'body',
+  )
+
+  def __init__(self):
+    for k in self.__slots__:
+      setattr(self, k, [])
+
+  def append(self, post_obj):
+    my_score = []
+    for top_level_comment in post_obj.comments:
+      if isinstance(top_level_comment, praw.models.MoreComments):
+        continue
+      self.id.append(top_level_comment.id)
+      self.link_id.append(top_level_comment.link_id)
+      self.post_id.append(post_obj.id)
+      my_score.append(top_level_comment.score)
+      self.body.append(top_level_comment.body)
+
+    mean_score = np.mean(my_score)
+    for score in my_score:
+      self.reward.append(score / post_obj.score)
+      self.regret.append((score - mean_score) / post_obj.score)
+
+    self.score.extend(my_score)
+
+  def to_dict(self):
+    return {
+        k: getattr(self, k)
+        for k in self.__slots__
     }
